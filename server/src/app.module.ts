f352d06e68c4d26406dc926246826f95e58f35d2
Hossein jalali <149144798+hachalick@users.noreply.github.com>
2024-09-02 23:37:16 +0330

    Create app.module.ts

---
+++ server/src/app.module.ts
@@ -0,0 +1,33 @@
+import { MiddlewareConsumer, Module } from '@nestjs/common';
+import { TypeOrmDbConfig } from "./modules/config/typeorm.config";
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CustomConfigsModule } from './modules/config/configs.module';
+import { LoggerMiddleware } from './app.middleware';
+import { OrderModule } from './order/order.module';
+import { MenuModule } from './menu/menu.module';
+import { BlogModule } from './blog/blog.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useClass: TypeOrmDbConfig,
+      inject: [CustomConfigsModule],
+    }),
+    CustomConfigsModule,
+    BlogModule,
+    OrderModule,
+    MenuModule,
+    AuthModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
