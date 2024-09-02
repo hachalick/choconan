600a491420a0dd3317fbda7b157a8aec16aa7243
Hossein jalali <149144798+hachalick@users.noreply.github.com>
2024-09-02 23:21:52 +0330

    Create Form1.cs

---
+++ Choconan/StartUp/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows.Forms;
+
+namespace StartUp
+{
+  public partial class FormStartUp : Form
+  {
+    public FormStartUp()
+    {
+      InitializeComponent();
+    }
+
+    private void buttonGoToAccount_Click(object sender, System.EventArgs e)
+    {
+      string url = "https://choconan.ir/account";
+      System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+      {
+        FileName = url,
+        UseShellExecute = true
+      });
+    }
+
+    private void buttonGoToAccountOrder_Click(object sender, System.EventArgs e)
+    {
+      string url = "https://choconan.ir/account/order";
+      System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+      {
+        FileName = url,
+        UseShellExecute = true
+      });
+    }
+  }
+}
