65228f5336cecbfa88b209528c04fff3c17e8bfc
Hossein jalali <149144798+hachalick@users.noreply.github.com>
2024-09-02 23:32:59 +0330

    Create fetch.type.ts

---
+++ client/src/types/fetch.type.ts
@@ -0,0 +1,73 @@
+type TProductMenu = {
+  id: number;
+  price: number;
+  waiting: number;
+  available: boolean;
+  meta_title: string;
+  meta_description: string;
+  name: string;
+  description: string;
+  src: string;
+};
+
+type TProductsMenu = TProductMenu[];
+
+type TIdProductMenu = TProductMenu & { product_id: string };
+
+type TIdProductsMenu = TIdProductMenu[];
+
+type TCategoryMenu = {
+  category: string;
+  icon: string;
+  products: TProductsMenu;
+};
+
+type TIdCategoryMenu = {
+  category_product_id: string;
+  category: string;
+  icon: string;
+  products: TIdProductMenu[];
+};
+
+type TIdCategoriesMenu = TIdCategoryMenu[];
+
+type TIdProductSearchMenu = TIdProductMenu & { rank: number; category: string };
+
+type TIdProductsSearchMenu = TIdProductSearchMenu[];
+
+type TIdFactorPresentOrder = {
+  factor_present_order_id: string;
+  count: number;
+  products: TIdProductMenu;
+};
+
+type TIdPresentOrderTable = {
+  present_order_table_id: string;
+  table: number;
+  busy: boolean;
+  accept: boolean;
+  factorPresentOrderTable: TIdFactorPresentOrder[];
+};
+
+type TIdPresentOrdersTable = TIdPresentOrderTable[];
+
+type TCreateBlog = { create: boolean; id: string };
+
+type TBlog = {
+  publish: boolean;
+  meta_title: string;
+  short_description: string | null;
+  title: string;
+  src_banner: string | null;
+  blog: string | null;
+};
+
+type TBlogs = TBlog[];
+
+type TIdBlog = TBlog & { blog_id: string };
+
+type TIdBlogs = TIdBlog[];
+
+type TGetIdBlog = TIdBlog & { create_at: Date; update_at: Date };
+
+type TGetIdBlogs = TGetIdBlog[];
