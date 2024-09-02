012b5ca609ff06259c940ec2bc1c33d63064b04a
Hossein jalali <149144798+hachalick@users.noreply.github.com>
2024-09-02 23:36:27 +0330

    Create menu.dto.ts

---
+++ server/src/menu/menu.dto.ts
@@ -0,0 +1,44 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class AddCategoryMenuDto {
+  @ApiProperty({ type: String, default: '' })
+  category: string;
+
+  @ApiProperty({ type: String, default: '' })
+  icon: string;
+}
+
+export class AddProductMenuDto {
+  @ApiProperty({ type: Number, default: 1 })
+  id: number;
+
+  @ApiProperty({ type: Boolean, default: true })
+  available: boolean;
+
+  @ApiProperty({ type: Number, default: 0 })
+  price: number;
+
+  @ApiProperty({ type: Number, default: 0 })
+  waiting: number;
+
+  @ApiProperty({ type: String, default: '' })
+  meta_title: string;
+
+  @ApiProperty({ type: String, default: '' })
+  meta_description: string;
+
+  @ApiProperty({ type: String, default: '' })
+  name: string;
+
+  @ApiProperty({ type: String, default: '' })
+  description: string;
+
+  @ApiProperty({ type: String, default: '' })
+  src: string;
+}
+
+export class UploadFileDto {
+  @ApiProperty({ type: 'string', format: 'binary' })
+  file: any;
+}
+
