5dc132d1104c97a8584e92c9ffe6c98a05e1020b
Hossein jalali <149144798+hachalick@users.noreply.github.com>
2024-09-04 14:21:57 +0330

    Update auth.dto.ts

--- server/src/auth/auth.dto.ts
+++ server/src/auth/auth.dto.ts
@@ -1,6 +1,26 @@
-import { ApiProperty } from "@nestjs/swagger";
+import { ApiProperty } from '@nestjs/swagger';
 
-export class OrderTableDto {
-  @ApiProperty({ default: [{ count: 1, product_id: '' }] })
-  list_order: { count: number; product_id: string }[];
+export class SignupDto {
+  @ApiProperty({ default: 'password | telegram-otp | sms-otp | email-otp' })
+  type: string;
+
+  @ApiProperty({ default: '98' })
+  national_code: string;
+
+  @ApiProperty({ default: '9XXXXXXXXX' })
+  phone: string;
+
+  @ApiProperty({ default: 'asd' })
+  password: string;
+}
+
+export class LoginPasswordDto {
+  @ApiProperty({ default: '98' })
+  national_code: string;
+
+  @ApiProperty({ default: '9XXXXXXXXX' })
+  phone: string;
+
+  @ApiProperty({ default: 'asd' })
+  password: string;
 }
