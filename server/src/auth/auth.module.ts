50476d6f735bf84427a6a16d5162156cfc79e0fd
Hossein jalali <149144798+hachalick@users.noreply.github.com>
2024-09-04 14:22:02 +0330

    Update auth.module.ts

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from 'src/modules/entity/mysql/User.entity';
+import { RoleEntity } from 'src/modules/entity/mysql/Role.entity';
+import { RoleUserEntity } from 'src/modules/entity/mysql/RoleUser.entity';
+import { JwtModule } from 'src/modules/jwt/jwt.module';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([UserEntity, RoleUserEntity, RoleEntity]),
+    JwtModule,
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
 })
 export class AuthModule {}
