a450431e9d8caebf2396b121a87540307ee31e02
Hossein jalali <149144798+hachalick@users.noreply.github.com>
2024-09-05 08:57:19 +0330

    Update auth.dto.ts

--- server/src/auth/auth.dto.ts
+++ server/src/auth/auth.dto.ts
@@ -1,26 +1,61 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsString, Length, MaxLength, MinLength } from 'class-validator';
 
 export class SignupDto {
-  @ApiProperty({ default: 'password | telegram-otp | sms-otp | email-otp' })
-  type: string;
-
-  @ApiProperty({ default: '98' })
+  @ApiProperty({ default: '98', type: 'string', minLength: 1, maxLength: 3 })
+  @IsString()
+  @MaxLength(3)
+  @MinLength(1)
   national_code: string;
 
-  @ApiProperty({ default: '9XXXXXXXXX' })
+  @ApiProperty({ default: '9XXXXXXXXX', minLength: 10, maxLength: 10 })
+  @IsString()
+  @Length(10)
   phone: string;
 
-  @ApiProperty({ default: 'asd' })
+  @ApiProperty({ default: 'asdf', minLength: 4 })
+  @IsString()
+  @MinLength(4)
   password: string;
 }
 
 export class LoginPasswordDto {
-  @ApiProperty({ default: '98' })
+  @ApiProperty({ default: '98', type: 'string', minLength: 1, maxLength: 3 })
+  @IsString()
+  @MaxLength(3)
+  @MinLength(1)
   national_code: string;
 
-  @ApiProperty({ default: '9XXXXXXXXX' })
+  @ApiProperty({ default: '9XXXXXXXXX', minLength: 10, maxLength: 10 })
+  @IsString()
+  @Length(10)
   phone: string;
 
-  @ApiProperty({ default: 'asd' })
+  @ApiProperty({ default: 'asdf', minLength: 4 })
+  @IsString()
+  @MinLength(4)
   password: string;
+}
+
+export class resetPasswordDto {
+  @ApiProperty({ default: '98', type: 'string', minLength: 1, maxLength: 3 })
+  @IsString()
+  @MaxLength(3)
+  @MinLength(1)
+  national_code: string;
+
+  @ApiProperty({ default: '9XXXXXXXXX', minLength: 10, maxLength: 10 })
+  @IsString()
+  @Length(10)
+  phone: string;
+
+  @ApiProperty({ default: 'asdf', minLength: 4 })
+  @IsString()
+  @MinLength(4)
+  old_password: string;
+
+  @ApiProperty({ default: 'asdf', minLength: 4 })
+  @IsString()
+  @MinLength(4)
+  new_password: string;
 }
