d6b5866dd60a30e47d2648a708e31e6955cf4f4c
Hossein jalali <149144798+hachalick@users.noreply.github.com>
2024-09-05 08:57:29 +0330

    Create message-http-exception.enum.ts

---
+++ server/src/modules/enum/message-http-exception.enum.ts
@@ -0,0 +1,10 @@
+export enum EMessageHttpException {
+  TOKEN_NOT_FOUND = 'توکن وجود ندارد',
+  TOKEN_IS_EXPIRES = 'توکن منقضی شده',
+  TOKEN_IS_NOT_EXPIRES = 'توکن منقضی نشده',
+  LOGIN_AGAIN = 'ورود مجدد',
+  USER_NOT_FOUND = 'کاربر وجود ندارد',
+  USER_FOUND = 'کاربر وجود دارد',
+  ACCESS_DENIED = 'دسترسی وجود ندارد',
+  PASSWORD_INCORRECT = 'رمز ورود اشتباه هست',
+}
