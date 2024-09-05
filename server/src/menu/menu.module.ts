7584501ac9108bba266f18274da123f00543bb54
Hossein jalali <149144798+hachalick@users.noreply.github.com>
2024-09-05 08:57:25 +0330

    Update menu.module.ts

--- server/src/menu/menu.module.ts
+++ server/src/menu/menu.module.ts
@@ -4,10 +4,13 @@ import { MenuService } from './menu.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CategoryProductMenuEntity } from 'src/modules/entity/mysql/Category-Product.entity';
 import { ProductMenuEntity } from 'src/modules/entity/mysql/Product.entity';
+import { JwtModule } from 'src/modules/jwt/jwt.module';
+import { UserEntity } from 'src/modules/entity/mysql/User.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([CategoryProductMenuEntity, ProductMenuEntity]),
+    TypeOrmModule.forFeature([CategoryProductMenuEntity, ProductMenuEntity, UserEntity]),
+    JwtModule,
   ],
   controllers: [MenuController],
   providers: [MenuService],
