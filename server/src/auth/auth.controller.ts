56daab8d1359c7653dc21004fc20a7e27f6930d4
Hossein jalali <149144798+hachalick@users.noreply.github.com>
2024-09-05 08:57:36 +0330

    Update auth.controller.ts

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -9,8 +9,13 @@ import {
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { ApiTags } from '@nestjs/swagger';
-import { LoginPasswordDto, SignupDto } from './auth.dto';
-import { CheckIsExpiresOtpGuard, CheckNotExpiresOtpGuard } from './auth.guard';
+import { LoginPasswordDto, resetPasswordDto, SignupDto } from './auth.dto';
+import {
+  LoginGuard,
+  RefreshTokenGuard,
+  ResetPasswordGuard,
+  SignUpGuard,
+} from './auth.guard';
 
 @ApiTags('Auth')
 @Controller('auth')
@@ -23,16 +28,33 @@ export class AuthController {
   }
 
   @Post('signup')
-  signup(@Body() body: SignupDto) {}
+  @UseGuards(SignUpGuard)
+  signup(@Body() body: SignupDto) {
+    const { national_code, password, phone } = body;
+    return this.authService.signupPassword({ national_code, password, phone });
+  }
 
   @Put('login-password')
+  @UseGuards(LoginGuard)
   loginPassword(@Body() body: LoginPasswordDto) {
     const { national_code, password, phone } = body;
     return this.authService.loginPassword({ national_code, password, phone });
   }
 
   @Get('refresh-token/:token')
+  @UseGuards(RefreshTokenGuard)
   refreshToken(@Param('token') token: string) {
     return this.authService.refreshToken({ token });
+  }
+
+  @Put('reset-password')
+  @UseGuards(ResetPasswordGuard)
+  resetPassword(@Body() body: resetPasswordDto) {
+    const { new_password, national_code, phone } = body;
+    return this.authService.resetPassword({
+      new_password,
+      national_code,
+      phone,
+    });
   }
 }
